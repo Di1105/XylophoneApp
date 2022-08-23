89d694701f19e0bfba4e18bc17363cda45aa472c
Dilara Elçioğlu <delcioglu@gmail.com>
2022-08-23 19:24:54 +0300

    push

--- XylophoneApp2/ViewController.swift
+++ XylophoneApp2/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var cAutlet: UIButton!    
+    @IBOutlet weak var bAutlet: UIButton!
+    
+    @IBOutlet weak var aAutlet: NSLayoutConstraint!
+    
+    @IBOutlet weak var gAutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cButton(_ sender: Any) {
+    }
+    @IBAction func bButton(_ sender: Any) {
+    }
+    @IBAction func aButton(_ sender: Any) {
+    }
+    @IBAction func gButton(_ sender: Any) {
+    }
+    @IBAction func fButton(_ sender: Any) {
+    }
+    @IBAction func eButton(_ sender: Any) {
+    }
+    @IBAction func dButton(_ sender: Any) {
+    }
+    
+    @IBAction func ccButton(_ sender: Any) {
+    }
+    
 }
 

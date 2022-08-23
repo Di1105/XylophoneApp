4a236d614448deb9041034477479a7bfcac3e137
mfsaglam <mfsaglam@yandex.com>
2022-08-23 20:51:50 +0300

    add xbutton class and nib file

--- XylophoneApp2/ViewController.swift
+++ XylophoneApp2/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var aButton: XButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        aButton.xlabelText = "A"
+        aButton.xcolor = .purple
         // Do any additional setup after loading the view.
     }
 
